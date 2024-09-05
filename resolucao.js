a452317041fa602f57e37d43905b1bdd743fcd64
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-09-05 16:44:03 -0300

    adicionada resolução do desafio proposto no README.md

---
+++ resolucao.js
@@ -0,0 +1,13 @@
+// Resolução do desafio Workshop JS Fatec Jahu
+// Nome: Gabriel Ficcio Botan
+// DSM - 2º Semestre
+
+function somar(a, b) { // Função que recebe dois parâmetros e retorna a soma deles
+  return a + b;
+}
+
+let x = 5; // let é uma variável que pode ser alterada
+const y = 5; // const é uma variável que não pode ser alterada
+x = x + 10; // x = 15
+
+console.log(somar(x, y)); // 15 + 5 = 20
